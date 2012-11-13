ff288230b351c40b24ca05183145338c867cf4cb
Michael Heu <mheu@clickbooq.com>
2012-11-12 19:43:52 -0500

    added sortable features

--- app.rb
+++ app.rb
@@ -6,23 +6,40 @@ DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/daily_task.db")
 class Task  
   include DataMapper::Resource  
   property :id, Serial  
+  property :position, Integer, default: 0
   property :content, Text, :required => true  
   property :complete, Boolean, :required => true, :default => false  
 end  
 
-DataMapper.finalize.auto_upgrade!
+DataMapper.finalize.auto_upgrade! # runs migration after adding new property
 
 get '/' do 
-	@tasks = Task.all order: :id.desc
-	# @title = 'Home'
+	@tasks = Task.all order: :position.asc
 	erb :index
 end
 
-post '/' do 
+post '/' do # create
+  # Increment all existing task positions by 1
+  tasks = Task.all
+  tasks.each do |task|
+    task.position = task.position + 1
+    task.save
+  end
+
+  #create new task with default position of 0
 	t = Task.new
 	t.content = params[:content]
 	t.save
+  
 	redirect '/'
+end
+
+post '/sort' do
+  params[:task].each_with_index do |task, index|
+    t = Task.get(task.to_i)
+    t.position = index
+    t.save # might be expensive 
+  end
 end
 
 get '/:id/complete' do  
--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
 	$( "#sortable" ).sortable({
-
+		update: function(e){
+			$.ajax({
+				type: 'POST',
+				url: '/sort',
+				data: $(this).sortable('serialize')
+			});
+		}
 	})
 	$( "#sortable span" ).disableSelection();
 });
